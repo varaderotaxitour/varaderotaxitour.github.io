f2803ef23d418bbbaa22d9e0f7ae2e892c23950b
BarbaroYoel <barbaroyoelmartinez@gmail.com>
2026-08-20 19:25:01 -0400

    chore: migrar a Vercel, eliminar workflow de GitHub Pages y actualizar config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  site: 'https://varaderotaxitour.github.io',
   integrations: [react()],
 });
